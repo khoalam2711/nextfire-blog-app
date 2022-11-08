27fa78fd323b59bf410d0864d5296c5e8931df78
KoahF <khoakg2013@gmail.com>
2022-11-08 15:02:08 +0700

    Config nextjs to accept images domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+				port: '',
+			},
+			{
+				protocol: 'https',
+				hostname: 'www.wycliffe.ca',
+				port: '',
+			},
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
