ceac0ce1d967aea4a3cb4b87fcc69b7ac151117a
KoahF <khoakg2013@gmail.com>
2022-12-15 20:10:27 +0700

    Fix tailwind bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
 		'./components/**/*.{js,ts,jsx,tsx}',
 		'./hooks/**/*.{js,ts,jsx,tsx}',
 		'./utils/**/*.{js,ts,jsx,tsx}',
+		'./styles/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
 		extend: {},
